64627a54f67967e3346a23b8044d35f24c093ce8
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-23 00:49:53 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,6 +1,6 @@
 class CreateCharacters < ActiveRecord::Migration[4.2]
   def change
-    belongs_to :actor
+    belongs_to :actors
     create_table :characters do |c|
       c.string :name
       c.string :catchphrase
